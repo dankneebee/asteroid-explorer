f287ceb4baab40c7432394b71f07104066558e9c
Daniela Georgieva <95629687+dankneebee@users.noreply.github.com>
2025-05-21 16:59:20 +0300

    Others

---
+++ wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿
+$(document).ready(function() {
+    // adds hover effect to cards
+    $('.card').hover(
+        function() {
+            $(this).addClass('shadow');
+        },
+        function() {
+            $(this).removeClass('shadow');
+        }
+    );
+
+    // adds click effect to buttons
+    $('.btn-primary').click(function() {
+        $(this).addClass('active');
+        setTimeout(() => {
+            $(this).removeClass('active');
+        }, 200);
+    });
+});
